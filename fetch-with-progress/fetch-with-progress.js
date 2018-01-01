f2a31e90856b666e36ba81513a9a11f171480a03
Vladimir <dosandkv@gmail.com>
2018-01-02 00:28:09 +0300

    finished fetch with progress;

--- fetch-with-progress/fetch-with-progress.js
+++ fetch-with-progress/fetch-with-progress.js
@@ -3,10 +3,34 @@ const $percentValue = document.getElementById('percent-value');
 const $requestBtn = document.getElementById('request');
 const $cancelBtn = document.getElementById('cancel');
 
+const $toggleRequestTypeBtn = document.getElementById('toggle-request-type-btn');
+const $progressControls = document.getElementById('progress-controls');
+const $urlInput = document.getElementById('url-input');
+
+$toggleRequestTypeBtn.addEventListener('click', () => {
+  $progressControls.classList.toggle('hide');
+});
+
 const request = {
+  get url () {
+    return $urlInput.value;
+  },
+
   async makeRequest () {
-    const url = 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Cosmic_%E2%80%98Winter%E2%80%99_Wonderland.jpg';
-    const response = await fetch(url);
+    const {checked} = $toggleRequestTypeBtn;
+    checked ? this.requestWithProgress() : this.basicRequest();
+  },
+
+  async basicRequest () {
+    const response = await fetch(this.url);
+    const blobData = await response.blob();
+
+    this.showLoadedImg(blobData);
+  },
+
+  async requestWithProgress () {
+    const chunksArr = [];
+    const response = await fetch(this.url);
     this.reader = response.body.getReader();
     const contentLength = response.headers.get('Content-Length');
 
@@ -20,13 +44,11 @@ const request = {
       const width = `${Math.round(received / contentLength * 100)}%`;
 
       if (done) {
-        request.progressAnimationId = requestAnimationFrame(() => {
-          $fill.style.background = 'green';
-          request.disableCancelBtn();
-          document.body.style.backgroundImage = `url(${url})`;
-        });
-        return;
+        const blobData = new Blob([concatenate(Uint8Array, ...chunksArr)]);
+        return request.finishProgress(blobData);
       }
+
+      chunksArr.push(value);
 
       if ($fill.style.width !== width) {
         requestAnimationFrame(() => {
@@ -56,6 +78,19 @@ const request = {
     $requestBtn.classList.add('disabled');
   },
 
+  finishProgress (blob) {
+    this.progressAnimationId = requestAnimationFrame(() => {
+      $fill.style.background = 'green';
+      request.disableCancelBtn();
+      this.showLoadedImg(blob);
+    });
+  },
+
+  showLoadedImg (blob) {
+    const imageUrl = window.URL.createObjectURL(blob);
+    document.body.style.backgroundImage = `url(${imageUrl})`;
+  },
+
   async abortRequest () {
     if (this.reader) {
       await this.reader.cancel();
@@ -64,3 +99,17 @@ const request = {
     }
   }
 };
+
+function concatenate (resultConstructor, ...arrays) {
+  let totalLength = 0;
+  for (let arr of arrays) {
+    totalLength += arr.length;
+  }
+  let result = new resultConstructor(totalLength);
+  let offset = 0;
+  for (let arr of arrays) {
+    result.set(arr, offset);
+    offset += arr.length;
+  }
+  return result;
+}
